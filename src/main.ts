546b7aeabfd8db7c02b4fee9e1de8ded9d68b055
belkud <vlad-belkud@yandex.ru>
2023-12-25 17:04:44 +0300

    jk

--- src/main.ts
+++ src/main.ts
@@ -103,13 +103,13 @@ const thirdLine = document.querySelector('#thirdLine') as HTMLDivElement
           
             setTimeout(()=> {
                 starSky.children[i].style.opacity = '1'
-                starSky.children[i].style.transition = 1+'s'
+                starSky.children[i].style.transition = 2+'s'
             },0)
             setTimeout(()=> {
                 starSky.children[i].style.opacity = '0'
-                starSky.children[i].style.transition = 1+'s'
+                starSky.children[i].style.transition = 2+'s'
                 
-            },1000)
+            },2000)
 
         }
     }, 5000)
